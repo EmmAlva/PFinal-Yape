6fb14f925ef77c5a54f458cca016b7ec6b56c47d
EmmAlva <clioemma_28@hotmail.com>
2017-06-22 10:26:34 -0500

    css 4 pagins

--- public/js/components/creauser.js
+++ public/js/components/creauser.js
@@ -1,33 +1,38 @@
 'use strict';
 
-const creaUser = () =>{
+const CreaUser = () =>{
 	const section = $('<section class="register"></section>');
 	const img = $('<img class="logo" src="img/icons/lockone.png">');
 	const h2 = $('<h2 class="title2">Crea tu usuario Yape</h2>');
 	const p = $('<p>Ingresa un nombre, email y clave de usuario<span class="codigo"></span></p>');
-	const div =$('<div class="ingreso"></div>');
-	const input = $('<input type="text">');
+	const div =$('<div class="form"></div>');
+	const input = $('<input type="text" placeholder ="Nombre">');
 	const icon = $('<img class= "icons lock" src="img/icons/user.png">');
 
-
-	const input2 = $('<input type="email">');
+	const div2 =$('<div class="form"></div>');
+	const input2 = $('<input type="email" placeholder="correo@ejemplo.com">');
 	const icon2 = $('<img class= "icons lock" src="img/icons/message-gray.png">');
 
-	const input3 = $('<input type="password" maxlength="6" pattern="[0-9]">');
+	const div3 =$('<div class="form"></div>');
+	const input3 = $('<input type="password" maxlength="6" pattern="[0-9]" placeholder="Ingresa clave de 6 dígitos">');
 	const icon3 = $('<img class= "icons lock" src="img/icons/lock.png">');
 
-	const span = $('<span>Cuida esta clave como oro,, es tu acceso a Yape</span>');
+	const span = $('<span>Cuida esta clave como oro, es tu acceso a Yape</span>');
 	
 	section.append(img);
 	section.append(h2);
 	section.append(p);
 	div.append(icon);
 	div.append(input);
-	div.append(icon2);
-	div.append(input2);
-	div.append(icon3);
-	div.append(input3);
+	div2.append(icon2);
+	div2.append(input2);
+	div3.append(icon3);
+	div3.append(input3);
 	section.append(div);
+	section.append(div2);
+	section.append(div3);
 	section.append(span);
+
+	return section;
 
 }
--- public/js/components/ingcodigo.js
+++ public/js/components/ingcodigo.js
@@ -3,7 +3,7 @@ const IngCodigo = () =>{
 	const section = $('<section class="register"></section>');
 	const img = $('<img class="logo" src="img/icons/message.png">');
 	const h2 = $('<h2 class="title2">Ahora ingresa tu codigo</h2>');
-	const p = $('<p>ENviamos un SMS con el código de validación al númer <span class="codigo"></span></p>');
+	const p = $('<p>Enviamos un SMS con el código de validación al número <span class="codigo"></span></p>');
 	const div =$('<div class="ingreso"></div>');
 	const input = $('<input type="number" maxlength="4" pattern="[0-9]">');
 	const icon = $('<img class= "icons lock" src="img/icons/lock.png">');
@@ -17,14 +17,14 @@ const IngCodigo = () =>{
 	section.append(div);
 	section.append(span);
 
-	if($('input').length, (e) =>{
-		e.preventDefault();
-		state.selectScreen = 2;//= no me sirve :/
-		$('section').replaceWith(creaUser());
-	});
-
-
-
+	if($('input').val().length == 4){
+		console.log($('input').val());
+		state.selectScreen = 4;//= no me sirve :/
+		setTimeout(2000);
+		$('section').replaceWith(CreaUser());
+		
+	}
+	
 
 	return section;
 }
